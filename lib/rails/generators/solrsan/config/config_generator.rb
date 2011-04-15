d6d68e369965fc5c593d6a855b6a279a9d8a3ac1
Tommy Chheng <tommy.chheng@gmail.com>
2011-04-15 10:44:19 -0700

    generator char fix

--- lib/rails/generators/solrsan/config/config_generator.rb
+++ lib/rails/generators/solrsan/config/config_generator.rb
@@ -3,15 +3,12 @@ require 'rails/generators/solrsan_generator'
 module Solrsan
   module Generators
     class ConfigGenerator < Rails::Generators::Base
-      desc "This setups the configuration files for solrsan"
-      def self.source_root
-        File.expand_path("../templates", __FILE__)
-      end
+      source_root File.expand_path("../templates", __FILE__)
 
-      def create_config_files
-        template "solrsan.rb", File.join("config", "initializers", "solrsan.rb")
-        template "solr.yml", File.join("config", "solr.yml")
-      end
+      def create_config_files
+        template("solrsan.rb", File.join("config", "initializers", "solrsan.rb"))
+        template("solr.yml", File.join("config", "solr.yml"))
+      end
 
       def copy_solr_conf
         Dir["config/solr/conf/*"].each do |source|
@@ -19,7 +16,7 @@ module Solrsan
           FileUtils.rm(destination) if options[:force]
           if File.exist?(destination)  
             puts "Skipping #{destination} because it already exists"
-          else  
+          else
             puts "Generating #{destination}"
             FileUtils.cp(source, destination)
           end
